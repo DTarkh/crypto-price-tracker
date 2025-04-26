8d7b2ce5dbdbf635b0265bbbe252644a0848346c
David Tarkhnishvili <datotarkh@gmail.com>
2025-04-26 12:43:24 +0400

    Added loading indicator

--- src/components/CoinTicker.tsx
+++ src/components/CoinTicker.tsx
@@ -1,5 +1,5 @@
 import { useBinanceMiniTicker } from "../useData";
-
+import { ThreeCircles } from "react-loader-spinner";
 type CoinTickerProps = {
   symbol: string;
 };
@@ -9,16 +9,22 @@ export default function CoinTicker({ symbol }: CoinTickerProps) {
 
   if (!ticker) {
     return (
-      <div className="flex items-center justify-center h-40">
-        <div className="text-gray-500 animate-pulse">
-          Loading {symbol} data...
-        </div>
+      <div className="w-[300px] h-[300px] mx-auto p-6 bg-white shadow-xl rounded-xl mt-8 flex items-center justify-center">
+        <ThreeCircles
+          visible={true}
+          height="40"
+          width="40"
+          color="#4fa94d"
+          ariaLabel="three-circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
       </div>
     );
   }
 
   return (
-    <div className="w-[300px] mx-auto p-6 bg-white shadow-xl rounded-xl mt-8">
+    <div className="w-[300px] h-[300px] mx-auto p-6 bg-white shadow-xl rounded-xl mt-8">
       <h2 className="text-2xl font-bold mb-4 text-center">{ticker.s}</h2>
 
       <div className="grid grid-cols-2 gap-4 text-gray-700">
