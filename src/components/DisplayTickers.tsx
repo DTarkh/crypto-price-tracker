808eb7610bda7304d4a3db59b11d290119082d29
David Tarkhnishvili <datotarkh@gmail.com>
2025-04-26 11:55:34 +0400

    Created component for displaying choosen tockers

---
+++ src/components/DisplayTickers.tsx
@@ -0,0 +1,29 @@
+
+import CoinTicker from "./CoinTicker";
+
+interface Props {
+  selectedCoins: string[];
+  handleRemoveCoin: (coin: string) => void;
+}
+
+export default function DisplayTickers({
+  selectedCoins,
+  handleRemoveCoin,
+}: Props) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+      {selectedCoins.map((coin) => (
+        <div key={coin} className="relative">
+          <CoinTicker symbol={coin} />
+          <button
+            onClick={() => handleRemoveCoin(coin)}
+            className="absolute top-2 right-2 bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center hover:bg-red-600 transition"
+            title="Remove"
+          >
+            ✖
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
