7bfa089020ffc77e697ae8d0b5c526afa4f354e9
David Tarkhnishvili <datotarkh@gmail.com>
2025-04-26 11:56:19 +0400

    Imported components in App, manage add/delete tickers functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import "./App.css";
+import { useState } from "react";
+import SelectCoin from "./components/SelectCoin";
+import Header from "./components/Header";
+import DisplayTickers from "./components/DisplayTickers";
 
 function App() {
-  return <>
-  <h2 className="text-red-600">fefe</h2>
-  </>;
+  const [selectedCoins, setSelectedCoins] = useState<string[]>([]);
+
+  const handleAddCoin = (symbol: string) => {
+    if (!selectedCoins.includes(symbol)) {
+      setSelectedCoins((prev) => [...prev, symbol]);
+    }
+  };
+
+  const handleRemoveCoin = (symbol: string) => {
+    setSelectedCoins((prev) => prev.filter((coin) => coin !== symbol));
+  };
+
+  return (
+    <div className="min-h-screen p-6">
+      <Header />
+      <SelectCoin handleAddCoin={handleAddCoin} />
+      <DisplayTickers
+        selectedCoins={selectedCoins}
+        handleRemoveCoin={handleRemoveCoin}
+      />
+    </div>
+  );
 }
 
 export default App;
