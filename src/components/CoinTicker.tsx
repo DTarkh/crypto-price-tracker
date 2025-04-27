98f66fc043f7bad0a9a68755895042a382f07a02
David Tarkhnishvili <datotarkh@gmail.com>
2025-04-27 13:24:32 +0400

    Updated height of ticket cards

--- src/components/CoinTicker.tsx
+++ src/components/CoinTicker.tsx
@@ -9,7 +9,7 @@ export default function CoinTicker({ symbol }: CoinTickerProps) {
 
   if (!ticker) {
     return (
-      <div className="w-[300px] h-[300px] mx-auto p-6 bg-[#0b201d] shadow-xl rounded-xl mt-8 flex items-center justify-center">
+      <div className="w-[300px] h-[285px] mx-auto p-6 bg-[#0b201d] shadow-xl rounded-xl my-8 flex items-center justify-center">
         <ThreeCircles
           visible={true}
           height="40"
@@ -24,7 +24,7 @@ export default function CoinTicker({ symbol }: CoinTickerProps) {
   }
 
   return (
-    <div className="w-[300px] h-[300px] mx-auto p-6 bg-[#0b201d] shadow-xl rounded-xl mt-8">
+    <div className="w-[300px] h-[285px] mx-auto p-6 bg-[#0b201d] shadow-xl rounded-xl my-8">
       <h2 className="text-2xl font-bold mb-4 text-center">{ticker.s}</h2>
 
       <div className="grid grid-cols-2 gap-4 text-gray-400">
